169e4abdf2aa7eeab5fb905576c85f50be264aa6
Zach Hill <zach.ac.hill@gmail.com>
2018-12-18 21:53:05 -0600

    Update geocode.py

--- geocode.py
+++ geocode.py
@@ -1,5 +1,11 @@
 """
-I am using geopy to geocode locations into lat/long pairs. Yeah, I could use Google Api for this as well, but I am trying to spread out costs. Geocode is free so we are using it. I have noticed taking geopy cords and feeding them to Google Distance Api might return slightly off responses from Google. But, the response is off by like 5 yards. Good enough for vehicle routing, but maybe not cruise missiles, or killer drones. Neither of those things I condone with the use of my code. However, geopy.geocoders request you put your name, organization, and/or app name so they can see who is generating traffic to their service. Please change "user_agent" to reflect who/what you are doing by using their services, you freeloader. Also, make sure you have your "invoices.csv" has a column named "Location" and it is in the same dir. 
+I am using geopy to geocode locations into lat/long pairs. Yeah, I could use Google Api for this as well, but I am trying to 
+spread out costs. Geocode is free so we are using it. I have noticed taking geopy cords and feeding them to Google Distance Api
+might return slightly off responses from Google. But, the response is off by like 5 yards. Good enough for vehicle routing, but 
+maybe not cruise missiles, or killer drones. Neither of those things I condone with the use of my code. However, geopy.geocoders
+request you put your name, organization, and/or app name so they can see who is generating traffic to their service. Please 
+change "user_agent" to reflect who/what you are doing by using their services, you freeloader. Also, make sure you have your 
+"invoices.csv" has a column named "Location" and it is in the same dir. 
 """
 
 from geopy.geocoders import Nominatim
@@ -21,7 +27,7 @@ def latlong():
     
 
 #this returns a list required by main app function create_data_model(): ... looks like im using the Google's OR-tools
-def cord():
+def locationscord():
     _locations = []
     colnames = [ 'name', 'latitude', 'longitude']
     df = pd.read_csv(file, names=colnames, header=0)
